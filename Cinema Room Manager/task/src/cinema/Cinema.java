689b074b8e3ac785d12d020bbaaad060403d2a65
Dexter <gusevsergey85@gmail.com>
2023-01-06 10:50:51 +0300

    Second stage coast seats

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,9 +1,16 @@
 package cinema;
 
+import java.util.Scanner;
+
 public class Cinema {
 
     public static void main(String[] args) {
         // Write your code here
+        getPrice();
+
+
+    }
+    public static void gerCinemaRoom(){
         String[][] cinemaRoom = {
                 {" ", "1", "2", "3", "4", "5", "6", "7", "8"},
                 {"1", "S", "S", "S", "S", "S", "S", "S", "S"},
@@ -21,5 +28,31 @@ public class Cinema {
             }
             System.out.println();
         }
+
+    }
+
+    public static void getPrice() {
+        int price60 = 10;
+        int priceFirstPart = 10;
+        int priceSecondPart = 8;
+        int totalCoast;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int rows = scan.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        int seats = scan.nextInt();
+        if (rows * seats <= 60) {
+            totalCoast = price60 * rows * seats;
+        } else {
+            if (rows % 2 == 0) {
+                totalCoast = rows / 2 * seats * priceFirstPart + rows / 2 * seats * priceSecondPart;
+            } else {
+                totalCoast = rows / 2 * seats * priceFirstPart + (rows / 2 + rows % 2) * seats * priceSecondPart;
+            }
+        }
+
+        System.out.println("Total income:");
+        System.out.println("$" + totalCoast);
+
     }
 }
